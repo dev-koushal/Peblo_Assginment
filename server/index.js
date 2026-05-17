dd802b0722ae5357e26e0f0e1f65bccc10614ae0
Koushal Yadav <137811402+dev-koushal@users.noreply.github.com>
2026-05-17 14:31:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://peblo-assginment.onrender.com/"],
   credentials: true,
 }));
 
